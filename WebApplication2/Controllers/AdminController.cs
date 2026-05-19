54fd7aa1ee059b2d47f8c183db88855b96858362
s224032194 <s224032194@M104-51>
2026-05-19 06:16:37 +0200

    Admin Functionality updated

--- WebApplication2/Controllers/AdminController.cs
+++ WebApplication2/Controllers/AdminController.cs
@@ -1,6 +1,30 @@
-﻿namespace WebApplication2.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
 {
-    public class AdminController
+    public class AdminController: Controller
     {
+
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult AddMedicalConditions()
+        {
+            return PartialView("_AddMedicalCOnditions");
+        }
+
+        public IActionResult AddAllergies()
+        {
+            return PartialView("_AddAllergies");
+        }
+
+        public IActionResult AddMedication()
+        {
+            return PartialView("_AddMedication");
+        }
     }
 }
--- WebApplication2/Controllers/PatientController.cs
+++ WebApplication2/Controllers/PatientController.cs
@@ -1,6 +1,7 @@
 ﻿namespace WebApplication2.Controllers
 {
-    public class PatientController
+    public class PatientController: Controller
     {
+
     }
 }
